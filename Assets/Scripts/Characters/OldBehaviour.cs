e127f19f3704b7f316e039dd2acc390cb10741aa
amir-toly <amir.toly@gmail.com>
2019-09-22 16:43:12 +1000

    Add Gumi, Man and Old characters

---
+++ Assets/Scripts/Characters/OldBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldBehaviour : MonoBehaviour, ICharacterable
+{
+    public AudioClip HopEffect;
+    public List<AudioClip> buckEffects;
+
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    void ICharacterable.HopAudioPlay()
+    {
+        _audioSource.PlayOneShot(HopEffect, 0.05f);
+    }
+
+    void ICharacterable.Killed()
+    {
+        throw new System.NotImplementedException();
+    }
+}
