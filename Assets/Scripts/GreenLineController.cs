b91ac71e9c33184264d987378662f42e884eb939
amir-toly <amir.toly@gmail.com>
2019-09-29 21:58:32 +1000

    Fix trees creation thanks to another co-trainee

--- Assets/Scripts/GreenLineController.cs
+++ Assets/Scripts/GreenLineController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class GreenLineController : MonoBehaviour
 {
+    private Vector3 _currentPosition;
     public List<GameObject> worldProps;
 
     // Awake is called when instantiating the object
     void Awake()
     {
+        _currentPosition = transform.position;
+
         foreach (int side in new int[] { -1, 1})
         {
             Instantiate(worldProps[Random.Range(0, 4)],
-                new Vector3((4.5f * side), 0, -0.45f),
+                new Vector3(
+                    (4.5f * side) + _currentPosition.x,
+                    0 + _currentPosition.y,
+                    -0.45f + _currentPosition.z
+                    ),
                 Quaternion.identity,
                 // Attached to the parent
                 this.transform);
