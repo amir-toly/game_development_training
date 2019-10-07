4c8650e731402d3d9255a144ee1b1014213f5670
amir-toly <amir.toly@gmail.com>
2019-10-07 18:39:27 +1100

    Load unlocked cars only

--- Assets/Scripts/RoadController.cs
+++ Assets/Scripts/RoadController.cs
@@ -20,10 +20,21 @@ public class RoadController : MonoBehaviour
         float distanceBetweenCarsInSeconds = Random.Range(2.2f, 3.7f);
         int carSpeed = Random.Range(2, 5);
         int moveDirection = new int[] { -1, 1 } [Random.Range(0, 2)];
+
+        List<int> unlockedVehiclesIndex = new List<int>();
+
+        for (int i = 0; i < vehicles.Count; i++)
+        {
+            Vehicle vehicle = vehicles[i];
+
+            if (vehicle.unlocked == true)
+                unlockedVehiclesIndex.Add(i);
+        }
+
         int carModel = Random.Range(0, vehicles.Count);
 
         // Will open a new thread
-        StartCoroutine(InstantiateCar(carModel, distanceBetweenCarsInSeconds, moveDirection, carSpeed));
+        StartCoroutine(InstantiateCar(unlockedVehiclesIndex[carModel % unlockedVehiclesIndex.Count], distanceBetweenCarsInSeconds, moveDirection, carSpeed));
     }
 
     private IEnumerator InstantiateCar(int carModel, float distanceInSeconds, int moveDirection, int speed)
