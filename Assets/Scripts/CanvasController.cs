a8279e99aac16ab9b2575b377d0be3c426c37d9c
amir-toly <amir.toly@gmail.com>
2019-11-29 09:15:37 +1100

    Add work from today's session: save and load score from disk

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -7,10 +7,14 @@ using UnityEngine.UI;
 public class CanvasController : MonoBehaviour, IPointerClickHandler
 {
     private Text _scoreNumber;
+    private Text _bestScore;
+    private Text _gameOverSessionScore;
 
     private void Awake()
     {
         _scoreNumber = GameObject.Find("/Canvas/ScoreNumber").GetComponent<Text>();
+        _bestScore = GameObject.Find("/Canvas/GameOverPanel/BestScoreNumber").GetComponent<Text>();
+        _gameOverSessionScore = GameObject.Find("/Canvas/GameOverPanel/SessionScoreNumber").GetComponent<Text>();
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -28,6 +32,10 @@ public class CanvasController : MonoBehaviour, IPointerClickHandler
 
     private void Update()
     {
-        _scoreNumber = GameObject.Find("/Canvas/ScoreNumber").GetComponent<Text>();
+        _scoreNumber.text = GameManager.Instance.Score.ToString();
+
+        //TODO: Put in the right place
+        _bestScore.text = GameManager.Instance.BestScore.ToString();
+        _gameOverSessionScore.text = GameManager.Instance.Score.ToString();
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using UnityEngine.UI;
+
+[Serializable]
+public struct GameStats
+{
+    public int bestScore;
+    public int availableCoins;
+    // public IDictionary<int, bool> characters;
+    // public int activeCharacter;
+
+    public GameStats(int bestScore, int availableCoins)
+    {
+        this.bestScore = bestScore;
+        this.availableCoins = availableCoins;
+    }
+}
 
 public class GameManager : Singleton<GameManager>
 {
     private Text _tapToPlay;
     private GameObject _gameOverPanel;
+    private GameObject _activeCharacter;
+
+    private Vector3 _charInitialLocation;
+    private Quaternion _charInitialRotation;
+    private Vector3 _charInitialScale;
 
     private int _sessionScore;
     private int _topScore;
+    private int _availableCoins;
 
     void Awake()
     {
@@ -17,6 +41,15 @@ public class GameManager : Singleton<GameManager>
         _gameOverPanel = GameObject.Find("/Canvas/GameOverPanel");
 
         InitialiseGame();
+    }
+
+    private void Start()
+    {
+        _activeCharacter = GameObject.FindGameObjectWithTag("Character");
+
+        _charInitialLocation = _activeCharacter.transform.position;
+        _charInitialRotation = _activeCharacter.transform.rotation;
+        _charInitialScale = _activeCharacter.transform.localScale;
     }
 
     public void InitialiseGame()
@@ -31,6 +64,11 @@ public class GameManager : Singleton<GameManager>
     {
         _tapToPlay.gameObject.SetActive(false);
 
+        _activeCharacter.transform.position = _charInitialLocation;
+        _activeCharacter.transform.rotation = _charInitialRotation;
+        _activeCharacter.transform.localScale = _charInitialScale;
+
+        _sessionScore = 0;
         isPlayerAlive = true;
 
         LoadGameData();
@@ -38,12 +76,52 @@ public class GameManager : Singleton<GameManager>
 
     private void LoadGameData()
     {
+        if (File.Exists(Application.persistentDataPath + "/gamesave.v1"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
 
+            // TODO: Create the try-catch block
+            FileStream file = File.Open(Application.persistentDataPath + "/gamesave.v1", FileMode.Open);
+            GameStats loadedGameStats = (GameStats) bf.Deserialize(file);
+            file.Close();
+
+            _topScore = loadedGameStats.bestScore;
+            _availableCoins = loadedGameStats.availableCoins;
+        }
+        else
+        {
+            // Assume it's the first time the game is executed
+            _topScore = 0;
+            _availableCoins = 0;
+        }
     }
 
     private void SaveGameData()
     {
+        FileStream file = null;
 
+        try
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+
+            file = File.Create(Application.persistentDataPath + "/gamesave.v1");
+
+            _topScore = Math.Max(_sessionScore, _topScore);
+
+            GameStats gameStats = new GameStats(_topScore, _availableCoins);
+
+            bf.Serialize(file, gameStats);
+        }
+        catch (Exception e)
+        {
+            //TODO: Remove this sh*t for production
+            Debug.Log(e.Message);
+        }
+        finally
+        {
+            if (file != null)
+                file.Close();
+        }
     }
 
     public void KillPlayer()
@@ -68,6 +146,14 @@ public class GameManager : Singleton<GameManager>
         {
             _sessionScore = Mathf.Max(value, _sessionScore);
             //_scoreNumber.text = _sessionScore.ToString();
+        }
+    }
+
+    public int BestScore
+    {
+        get
+        {
+            return _topScore;
         }
     }
 }
