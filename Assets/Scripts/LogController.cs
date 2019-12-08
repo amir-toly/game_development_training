3a668fb8ea3e37741fe2f10b078d5f7b5d63b4b0
amir-toly <amir.toly@gmail.com>
2019-12-09 09:10:40 +1100

    Add work from today's session: add logs on the river and accelerate/decelerate depending on where they are

---
+++ Assets/Scripts/LogController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogController : MonoBehaviour
+{
+    [HideInInspector] public float logSpeed;
+
+    private Vector3 _currentLogPosition;
+    private int _logDirection;
+
+    private float _currentSpeed;        // log speed
+    private float _maxSpeed;            // maximum speed of log at any time
+    private float _acceleration = 7f;   // how fast the log will accelerate/decelerate from central speed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentLogPosition = transform.position;
+        _currentSpeed = 5f * Mathf.Sign(logSpeed);
+        _maxSpeed = 6f * Mathf.Sign(logSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (logSpeed > 0)
+        {
+            if (_currentLogPosition.x >= -5 && _currentLogPosition.x < 4)
+            {
+                if (_currentSpeed > logSpeed)
+                    _currentSpeed -= _acceleration * Time.deltaTime;
+            }
+            else if (_currentLogPosition.x >= 4 && _currentLogPosition.x < 11)
+            {
+                if (_currentSpeed < _maxSpeed)
+                    _currentSpeed += _acceleration * Time.deltaTime;
+            }
+            else if (_currentLogPosition.x >= 11)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        else // Now we move in negative speed
+        {
+            if (_currentLogPosition.x <= 5 && _currentLogPosition.x > -4)
+            {
+                if (_currentSpeed < logSpeed)
+                    _currentSpeed += _acceleration * Time.deltaTime;
+            }
+            else if (_currentLogPosition.x <= -4 && _currentLogPosition.x > -11)
+            {
+                if (_currentSpeed > _maxSpeed)
+                    _currentSpeed -= _acceleration * Time.deltaTime;
+            }
+            else if (_currentLogPosition.x <= -11)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        // Increment the X position with the speed increment in units
+        _currentLogPosition.x += _currentSpeed * Time.deltaTime;
+
+        transform.position = _currentLogPosition;
+    }
+}
--- Assets/Scripts/RiverController.cs
+++ Assets/Scripts/RiverController.cs
@@ -2,8 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct Log
+{
+    public string size;
+    public GameObject prefab;
+}
+
 public class RiverController : MonoBehaviour
 {
+    [SerializeField] private List<Log> logs;
+
     private ParticleSystem _waterSplash;
     private AudioSource _splashSound;
 
@@ -11,6 +20,32 @@ public class RiverController : MonoBehaviour
     {
         _waterSplash = GetComponentInChildren<ParticleSystem>();
         _splashSound = GetComponentInChildren<AudioSource>();
+
+        float logSpeed = Random.Range(1f, 3f);
+        int moveDirection = new int[] { -1, 1 } [Random.Range(0, 2)];
+
+        StartCoroutine(InstantiateLog(moveDirection, logSpeed));
+    }
+
+    private IEnumerator InstantiateLog(int direction, float speed)
+    {
+        Vector3 sideToStart = new Vector3(-10 * direction, 0.1f, 0);
+
+        yield return new WaitForSeconds(Random.Range(0f, 2.5f));
+
+        while (true)
+        {
+            int size = Random.Range(0, logs.Count);
+
+            GameObject newLog = Instantiate(
+                logs[size].prefab,
+                transform.position + sideToStart,
+                Quaternion.identity,
+                transform);
+            newLog.GetComponent<LogController>().logSpeed = speed * direction;
+
+            yield return new WaitForSeconds(Random.Range(2f, 4.5f));
+        }
     }
 
     // Start is called before the first frame update
